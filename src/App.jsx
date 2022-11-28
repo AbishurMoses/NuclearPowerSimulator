4abcad00a5b8a1c9d37fad9038afd0f77a834821
Abishur Moses <abishur@gmail.com>
2022-11-28 18:09:41 -0500

    Major changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,126 @@
-import Navbar from "./components/Navbar"
 import "./App.css"
 import { useEffect } from "react"
 import { useState } from "react"
+import Navbar from "./components/Navbar"
 import Plant from "./components/Plant"
+// import Chart from 'chart.js/auto';
+import { Line } from "react-chartjs-2";
+
 const App = () => {
   const [data, setData] = useState({
     plant_name: "",
     reactors: [],
   })
 
-  // const [temp, setTemp] = useState({
-  //   temperature: {},
-  // })
+  const [formData, setFormData] = useState({
+    search: "",
+  })
 
-  useEffect(() => {
-    (async () => {
-      const raw = await fetch("https://nuclear.dacoder.io/reactors?apiKey=aff16bb6a30addb7")
-      const jsonData = await raw.json()
-      setData(jsonData)
-    })()
-  }, [])
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    })
+  } 
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     (async () => {
-  //       const raw = await fetch("https://nuclear.dacoder.io/reactors/temperature/e2358f4e-5f97-4176-addc-c6178516e81c?apiKey=aff16bb6a30addb7")
-  //       const jsonData = await raw.json()
-  //       setTemp(jsonData)
-  //       console.log(jsonData)
-  //     })()
-  //   }, 1000);
-  // }, {})
+    useEffect(() => {
+      const getData = async () => {
+        const raw = await fetch("https://nuclear.dacoder.io/reactors?apiKey=aff16bb6a30addb7")
+        const jsonData = await raw.json()
+        jsonData.reactors = await Promise.all(jsonData.reactors.map(async reactor => {
+          const rawTempData = await fetch(`https://nuclear.dacoder.io/reactors/temperature/${reactor.id}?apiKey=aff16bb6a30addb7`)
+          const tempData = await rawTempData.json()
 
-  return (
-    <div>
-      <Navbar />
-      <h1>{data.plant_name}</h1>
-      <div className="plants-container" style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-      }}>
-        <div style={{
+          const rawCoolantData = await fetch(`https://nuclear.dacoder.io/reactors/coolant/${reactor.id}?apiKey=aff16bb6a30addb7`)
+          const coolantData = await rawCoolantData.json()
+
+          const rawFuelLevelData = await fetch(`https://nuclear.dacoder.io/reactors/fuel-level/${reactor.id}?apiKey=aff16bb6a30addb7`)
+          const fuelLevelData = await rawFuelLevelData.json()
+
+          const rawReactorStateData = await fetch(`https://nuclear.dacoder.io/reactors/reactor-state/${reactor.id}?apiKey=aff16bb6a30addb7`)
+          const reactorStateData = await rawReactorStateData.json()
+
+          const rawRodsData = await fetch(`https://nuclear.dacoder.io/reactors/rod-state/${reactor.id}?apiKey=aff16bb6a30addb7`)
+          const rodsData = await rawRodsData.json()
+          return {
+            ...reactor,
+            temperature: tempData.temperature,
+            coolant: coolantData.coolant,
+            fuelLevel: fuelLevelData.fuel,
+            state: reactorStateData.state,
+            rodState: rodsData.control_rods,
+          }
+        }))
+        // console.log(jsonData)
+        setData(jsonData)
+      }
+
+      const dataIntervalTimer = setInterval(async () => {
+        await getData()
+      }, 250)
+
+      return () => {
+        clearInterval(dataIntervalTimer)
+      }
+    }, [])
+
+    useEffect(() => {
+      console.log(data)
+    })
+
+    return (
+      <div>
+        <Navbar />
+        <h1>{data.plant_name}</h1>
+        <div className="plants-container" style={{
           display: "flex",
           flexDirection: "row",
-          gap: "25px",
+          alignItems: "center",
         }}>
-          {
-            data.reactors.map(item => {
-              return (
-                <Plant
-                  key={item.id}
-                  name={item.name}
-                  // temp={item.temp}
-                />
-              )
-            })
-          }
+          <div style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "25px",
+          }}>
+            {
+              data.reactors.map(reactor => {
+                return (
+                  <Plant
+                    key={reactor.id}
+                    name={reactor.name}
+                    temperature={reactor.temperature}
+                    coolant={reactor.coolant}
+                    fuelLevel={reactor.fuelLevel}
+                    state={reactor.state}
+                    rodState={reactor.rodState}
+                  />
+                )
+              })
+            }
+            {/* <Line
+            data={{
+              // x-axis label values
+              labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+              datasets: [
+                {
+                  label: "# of Calories Lost",
+                  // y-axis data plotting values
+                  data: [200, 300, 1300, 520, 2000, 350, 150],
+                  fill: false,
+                  borderWidth: 4,
+                  backgroundColor: "rgb(255, 99, 132)",
+                  borderColor: 'green',
+                  responsive: true
+                },
+              ],
+            }}
+          /> */}
+          </div>
         </div>
-      </div>
-    </div >
-  )
-}
-
-export default App
+      </div >
+    )
+  }
+  export default App
--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,7 +1,6 @@
 import Nuclear from "./nuclear.jpg";
 
-const Plant = ({ name }) => {
-    // console.log(temp)
+const Plant = ({ name, temperature, coolant, fuelLevel, state, rodState }) => {
     return (
         <div style={{
             backgroundColor: "#E6E6E6",
@@ -9,16 +8,76 @@ const Plant = ({ name }) => {
             flexDirection: "column",
             // alignItems: "center",
             borderRadius: "5%",
+            // flexBasis: "33.33%",
         }}>
-            <img src={Nuclear} alt="Nuclear Power Plant"/>
+            <h3 style={{
+                display: "flex",
+                justifyContent: "center"
+            }}>{name}</h3>
+            <img src={Nuclear} alt="Nuclear Power Plant" style={{
+                borderRadius: "5px",
+            }} />
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                padding: "0px 15px 0px 15px",
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    gap: "5px",
+                }}>
+                    <p>{temperature.amount.toFixed(2)} {temperature.unit}</p>
+                    {
+                        Number(temperature.amount.toFixed(2)) === 22.22 ? (
+                            <p>🥶</p>
+                        ) : (
+                            <p>😊</p>
+                        )
+                    }
+                </div>
+            </div>
             <div style={{
                 display: "flex",
                 flexDirection: "row",
                 justifyContent: "space-between",
                 padding: "0px 15px 0px 15px",
             }}>
-                <p>{name}</p>
-                {/* <p>{amount}</p> */}
+                <p>Coolant: {coolant}</p>
+                {
+                    Number(fuelLevel.percentage.toFixed(2)) === 0 ? (
+                        <p>Fuel-Level: Empty</p>
+                    ) : (
+                        <p>Fuel-Level: {fuelLevel.percentage.toFixed(2)}</p>
+                    )
+                }
+            </div>
+            <div className="progress" style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                padding: "0px 15px 0px 15px",
+            }}>
+                <div className="progress-bar progress-bar-striped" role="progressbar" aria-label="Default striped example" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style={{ width: "200px", height: "10px", border: "1px solid black" }}></div>
+            </div>
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                padding: "0px 15px 0px 15px"
+            }}>
+                <p>Reactor State: {state}</p>
+            </div>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                padding: "0px 15px 0px 15px",
+            }}>
+                <p>Rods in: {rodState.in}</p>
+                <p>Rods out: {rodState.out}</p>
             </div>
         </div>
     )
