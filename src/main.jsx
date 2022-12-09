d8cec089600fe0a13408ea62d51e04cd9c8e6924
ahumanchild <essyfirst@gmail.com>
2022-12-09 12:59:01 -0500

    had to add some imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { SnackbarProvider } from 'notistack'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
