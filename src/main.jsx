767467f1c67080140359dc6fca2f65521aa7c37d
Abishur Moses <abishur@gmail.com>
2024-03-07 10:51:12 -0500

    MVP. New Project needs to be added

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
 import App from './App'
-<<<<<<< HEAD
-=======
 import { SnackbarProvider } from 'notistack'
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
 } from "react-router-dom";
->>>>>>> 93d84edb502ac4b78f3639d83bfd3f0f08d1a073
 import './index.css'
 import GalleryItem from './routes/GalleryItem';
-import { SnackbarProvider } from 'notistack';
 
 const router = createBrowserRouter([
   {
@@ -36,10 +26,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <RouterProvider router={router} />
     </SnackbarProvider>
   </React.StrictMode>
-)
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-  },
-]);
+);
